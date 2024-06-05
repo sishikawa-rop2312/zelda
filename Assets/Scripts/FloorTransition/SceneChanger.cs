26643508605c1c5799e43a76d53135e1b63e70e8
sishikawa-rop2312 <155729870+sishikawa-rop2312@users.noreply.github.com>
2024-06-05 20:55:39 +0900

    Scene切り替えバグ修正

--- Assets/Scripts/FloorTransition/SceneChanger.cs
+++ Assets/Scripts/FloorTransition/SceneChanger.cs
@@ -11,19 +11,50 @@ public class SceneChanger : MonoBehaviour
     // プレイヤーの出現位置
     public Vector3 spawnPosition;
 
+    // ワープ機能が有効かどうかを示すフラグ
+    bool canWarp = false;
+
+    // プレイヤーの初期位置
+    Vector3 playerInitialPosition;
+
+    // プレイヤーオブジェクト
+    GameObject player;
+
+    void Start()
+    {
+        // プレイヤーオブジェクトを探して初期位置を取得
+        player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            playerInitialPosition = player.transform.position;
+        }
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            // プレイヤーが初期位置から1f以上離れているか確認
+            if (Vector3.Distance(player.transform.position, playerInitialPosition) >= 1f)
+            {
+                canWarp = true;
+            }
+        }
+    }
+
     // トリガーに触れた際に呼び出される関数
     void OnTriggerEnter2D(Collider2D other)
     {
         // キャラクターがトリガーゾーンに入ったかを確認
-        if (other.CompareTag("Player"))
+        if (canWarp && other.CompareTag("Player"))
         {
             StartCoroutine(CheckPlayerMoving(other));
         }
     }
 
-    IEnumerator CheckPlayerMoving(Collider2D player)
+    IEnumerator CheckPlayerMoving(Collider2D playerCollider)
     {
-        PlayerController playerController = player.GetComponent<PlayerController>();
+        PlayerController playerController = playerCollider.GetComponent<PlayerController>();
         while (playerController.isMoving)
         {
             yield return null; // 次のフレームまで待機
@@ -40,10 +71,16 @@ public class SceneChanger : MonoBehaviour
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         // Playerオブジェクトを探して位置を更新
-        GameObject player = GameObject.FindWithTag("Player");
+        player = GameObject.FindWithTag("Player");
         if (player != null)
         {
             player.transform.position = spawnPosition;
+
+            // プレイヤーの初期位置を更新
+            playerInitialPosition = player.transform.position;
+
+            // ワープ機能を再度無効にする
+            canWarp = false;
         }
 
         // イベントからこの関数を削除
