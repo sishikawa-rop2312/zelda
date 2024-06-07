bb16c8ed64e1d325b4718f99d8f3216dca800d3f
harukasato0215 <popo02150125@gmail.com>
2024-06-07 16:09:48 +0900

    スライム修正中

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -1,240 +1,321 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using Unity.VisualScripting;
-// using UnityEngine;
-// using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class Collider2DEvent : UnityEvent<Collider2D> { }
-// public class SlimeController : MonoBehaviour
-// {
-//     //HP
-//     public int enemyHp = 1;
-//     // 移動速度
-//     public float moveSpeed = 0.2f;
+public class SlimeController : MonoBehaviour
+{
+    // 基本攻撃力
+    public int attackPower = 1;
+    // 1マス当たりの移動速度
+    public float moveSpeed = 0.2f;
+    // 索敵範囲
+    public float searchRange = 10f;
+    // 攻撃時クールタイム
+    public float attackCoolTime = 3;
+    // 障害物レイヤー
+    public LayerMask detectionMask;
+    // 敵レイヤー
+    public LayerMask PlayerMask;
+    // 攻撃エフェクト
+    public ParticleSystem attackEffect;
 
-//     // プレイヤーのTransform
-//     private Transform player;
-//     //敵を索敵
-//     private bool seach = false;
+    // プレイヤーとの距離
+    float distanceToPlayer;
 
-//     Animator animator;
-//     private DealDamage dealDamage;
-//     private slimeSeach slimeSeach;
+    // プレイヤーが索敵範囲内にいるか
+    bool isPlayerNearby = false;
 
-//     //索敵範囲外の時にコルーチンを呼び出しているか
-//     bool runActive = false;
-//     //索敵コルーチンが動いているか
-//     bool seachrun = false;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    DealDamage dealDamage;
+    Vector3 currentDirection = Vector3.zero;
+    bool isMoving = false;
+    public Transform playerTransform;
 
-//     //索敵範囲外の時の敵の進行間隔
-//     public float interval = 1.1f;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        dealDamage = GetComponent<DealDamage>();
+    }
 
-//     public LayerMask obstacleMask;
+    void Update()
+    {
+        SetSprite();
 
+        if (dealDamage.isDead)
+        {
+            // 何も行動しない
+        }
+        else
+        {
+            StartCoroutine(SearchPlayer(moveSpeed));
+            Debug.Log("isPlayerNearby:" + isPlayerNearby + ",isMoving:" + isMoving);
 
-//     void Start()
-//     {
-//         player = GameObject.FindGameObjectWithTag("Player").transform;
-//         this.animator = GetComponent<Animator>();
-//         dealDamage = GetComponent<DealDamage>();
+            //　距離が1マス以内(隣マス)なら攻撃
+            if (distanceToPlayer <= 1f && !isMoving)
+            {
+                // ResetAnimation();
+                Debug.Log(gameObject.name + "は攻撃メソッドを呼び出します(" + distanceToPlayer + ")");
+                StartCoroutine(Attack());
+            }
+            // 索敵範囲内だが2マス以上離れている場合は追跡
+            else if (isPlayerNearby && !isMoving)
+            {
+                // プレイヤーに向かって移動
+                Vector3 directionToPlayer = (playerTransform.position - transform.position).normalized;
+                Debug.Log(gameObject.name + "はプレイヤーに向かって移動します");
+                MoveDirection(directionToPlayer);
+            }
+            // 索敵範囲内に見つからなければランダム
+            else if (!isMoving)
+            {
+                // ランダムな方向に移動
+                Vector3 randomDirection = Random.insideUnitCircle.normalized;
+                Debug.Log(gameObject.name + "はランダムに移動します");
+                MoveDirection(randomDirection);
 
-//         LayerMask obstacleMask = LayerMask.GetMask("Obstacle");
-//         collisionEventDispacher2D._OnColliderEvent.AddListener(OnColliderEventTriggered);
+                isMoving = false;
+            }
+            // 万が一、何にも当てはまらなければ何もしない
+            else
+            {
+                Debug.Log(gameObject.name + "は何もすることがありません(" + isMoving + ", " + isPlayerNearby + ")");
+            }
+        }
 
+    }
 
-//     }
-
-
-//     void Update()
-//     {
-
-//         if (seach && seachrun == false)
-//         {
-//             StartCoroutine(Chase());
-
-
-//         }
-//         else if (runActive == false)
-//         {
-//             StartCoroutine(RandomRun());
-//         }
-
-
-
-//     }
-
-//     IEnumerator RandomRun()
-//     {
-//         while (!seach)
-//         {
-//             runActive = true;
-
-//             Run();
-//             yield return new WaitForSeconds(interval);
-//             Debug.Log("停止中");
-
-//         }
-
-//     }
-
-
-
-//     //プレイヤーが索敵範囲にいない時
-//     void Run()
-//     {
-//         //ランダムに動くようにする
-//         int randomRun = Random.Range(0, 4);
-//         switch (randomRun)
-//         {
-//             case 0:
-//                 // 上
-//                 animator.SetTrigger("Up");
-//                 transform.position += new Vector3(0, moveSpeed, 0);
-
-//                 Debug.Log("上");
-//                 break;
-//             case 1:
-//                 // 下
-//                 animator.SetTrigger("Down");
-//                 transform.position += new Vector3(0, -moveSpeed, 0);
-//                 Debug.Log("下");
-
-//                 break;
-//             case 2:
-//                 // 右
-//                 animator.SetTrigger("Right");
-//                 transform.position += new Vector3(moveSpeed, 0, 0);
-//                 Debug.Log("右");
-
-//                 break;
-//             case 3:
-//                 // 左
-//                 animator.SetTrigger("Left");
-//                 transform.position += new Vector3(-moveSpeed, 0, 0);
-//                 Debug.Log(" 左");
-
-//                 break;
-//         }
-//     }
+    IEnumerator SearchPlayer(float interval)
+    {
+        // プレイヤーとの距離を計測
+        distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
+        // プレイヤーが索敵範囲内にいるかチェック
+        isPlayerNearby = distanceToPlayer <= searchRange;
+        yield return new WaitForSeconds(interval);
+    }
 
 
-//     IEnumerator Chase()
-//     {
-//         while (seach)
-//         {
-//             seachrun = true;
-//             MoveTowardsPlayer();
-//             yield return new WaitForSeconds(interval);
+    public Sprite up;
+    public Sprite down;
+    public Sprite left;
+    public Sprite right;
+    void SetSprite()
+    {
+        if (currentDirection == Vector3.up)
+        {
+            // 上向きのスプライトを設定
+            spriteRenderer.sprite = up;
+        }
+        else if (currentDirection == Vector3.down)
+        {
+            // 下向きのスプライトを設定
+            spriteRenderer.sprite = down;
+        }
+        else if (currentDirection == Vector3.left)
+        {
+            // 左向きのスプライトを設定
+            spriteRenderer.sprite = left;
+        }
+        else if (currentDirection == Vector3.right)
+        {
+            // 右向きのスプライトを設定
+            spriteRenderer.sprite = right;
+        }
+    }
 
+    void WalkAnimation(string animation)
+    {
+        if (animation == "WalkDown")
+        {
+            animator.SetTrigger("Down");
 
-//         }
-//     }
+        }
+        if (animation == "WalkUp")
+        {
+            animator.SetTrigger("Up");
 
+        }
+        if (animation == "WalkLeft")
+        {
 
-//     void MoveTowardsPlayer()
-//     {
+            animator.SetTrigger("Left");
 
+        }
+        if (animation == "WalkRight")
+        {
+            animator.SetTrigger("Right");
 
+        }
+    }
 
+    void MoveDirection(Vector3 direction)
+    {
+        // 斜め移動を制限し、進行方向を変える
+        if (direction.x != 0 && direction.y != 0)
+        {
+            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+            {
+                direction.y = 0;
 
-//         // プレイヤーの方向を向く（正面のみで実装）
-//         Vector3 direction = player.position - transform.position;
+                if (direction.x > 0)
+                {
+                    direction = Vector3.right;
+                }
+                else
+                {
+                    direction = Vector3.left;
+                }
+            }
+            else
+            {
+                direction.x = 0;
 
-//         RaycastHit2D hitX = Physics2D.Raycast(transform.position, Vector2.right, 1f, detectionMask);
-//         RaycastHit2D hitY = Physics2D.Raycast(transform.position, Vector2.up, 1f, detectionMask);
+                if (direction.y > 0)
+                {
+                    direction = Vector3.up;
+                }
+                else
+                {
+                    direction = Vector3.down;
+                }
+            }
+        }
 
-//         if (hitX.collider == null || hitY.collider == null)
-//         {
-//             Debug.Log("障害物がない！");
-//             if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
-//             {
-//                 // 横方向の距離が縦方向より大きい場合、横方向に移動
-//                 direction = new Vector3(Mathf.Sign(direction.x), 0, 0);
-//                 if (direction.x > 0)
-//                 {
-//                     animator.SetTrigger("Right");
-//                     transform.position += new Vector3(moveSpeed, 0, 0);
-//                 }
-//                 else
-//                 {
-//                     animator.SetTrigger("Left");
-//                     transform.position += new Vector3(-moveSpeed, 0, 0);
-//                 }
-//             }
-//             else
-//             {
-//                 // 縦方向の距離が横方向より大きい場合、縦方向に移動
-//                 direction = new Vector3(0, Mathf.Sign(direction.y), 0);
-//                 if (direction.y > 0)
-//                 {
-//                     animator.SetTrigger("Up");
-//                     transform.position += new Vector3(0, moveSpeed, 0);
-//                 }
-//                 else
-//                 {
-//                     animator.SetTrigger("Down");
-//                     transform.position += new Vector3(0, -moveSpeed, 0);
-//                 }
-//             }
+        // 現在の向きを設定
+        currentDirection = direction;
 
-//         }
-//         else if (hitX.collider == null)
-//         {
-//             Debug.Log("左右どちらかに障害物がある");
-//             int randomnum = Random.Range(0, 2);
-//             if (randomnum == 0)
-//             {
-//                 animator.SetTrigger("Up");
-//                 transform.position += new Vector3(0, moveSpeed, 0);
-//             }
-//             else
-//             {
-//                 animator.SetTrigger("Down");
-//                 transform.position += new Vector3(0, -moveSpeed, 0);
-//             }
+        // アニメーション再生
+        string animation;
+        if (currentDirection == Vector3.up) { animation = "WalkUp"; }
+        else if (currentDirection == Vector3.left) { animation = "WalkLeft"; }
+        else if (currentDirection == Vector3.right) { animation = "WalkRight"; }
+        else { animation = "WalkDown"; }
+        WalkAnimation(animation);
 
-//         }
-//         else
-//         {
-//             Debug.Log("上下どちらかに障害物がある");
-//             int randomnum = Random.Range(0, 2);
-//             if (randomnum == 0)
-//             {
-//                 animator.SetTrigger("Right");
-//                 transform.position += new Vector3(moveSpeed, 0, 0);
-//             }
-//             else
-//             {
-//                 animator.SetTrigger("Left");
-//                 transform.position += new Vector3(-moveSpeed, 0, 0);
-//             }
-//         }
+        // 進行方向に障害物がないかチェック
+        if (Physics2D.OverlapPoint(transform.position + currentDirection, detectionMask) != null || Physics2D.OverlapPoint(transform.position + currentDirection, PlayerMask) != null)
+        {
+            isMoving = false;
+        }
+        else // なければMove呼び出し
+        {
+            StartCoroutine(Move(animation));
+        }
+    }
 
+    IEnumerator Move(string animation)
+    {
+        // 行動中フラグを立てる
+        isMoving = true;
+        // 現在地を取得
+        Vector3 nowPosition = transform.position;
+        // 目的地を取得
+        Vector3 targetPosition = nowPosition + currentDirection;
+        float elapsedTime = 0f;
 
-//     }
+        // 移動速度に合わせて移動
+        while (elapsedTime < moveSpeed)
+        {
+            transform.position = Vector3.Lerp(nowPosition, targetPosition, (elapsedTime / moveSpeed));
+            elapsedTime += Time.deltaTime;
 
+            yield return null;
+        }
 
-//     public void TakeDamage(int damage)
-//     {
-//         dealDamage.Damage(damage);
-//     }
+        // 万が一、移動しすぎor間に合わないときは強制移動
+        transform.position = targetPosition;
 
+        // 移動中フラグオフ
+        isMoving = false;
+    }
 
-//     IEnumerator AttackPlayer()
-//     {
-//         Debug.Log("スライムはプレイヤーに攻撃した！");
-//         yield return new WaitForSeconds(10);
+    IEnumerator Attack()
+    {
+        // 敵の位置を取得
+        Vector3 direction = (playerTransform.position - transform.position).normalized;
 
-//     }
-//     bool attackActive = false;
+        // 敵の方を向く
+        if (direction.x != 0 || direction.y != 0)
+        {
+            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+            {
+                direction.y = 0;
 
-//     private void OnTriggerEnter2D(Collider2D collision)
-//     {
+                if (direction.x > 0)
+                {
+                    direction = Vector3.right;
+                }
+                else
+                {
+                    direction = Vector3.left;
+                }
+            }
+            else
+            {
+                direction.x = 0;
 
-//         if (collision.CompareTag("Player") && !attackActive)
-//         {
-//             StartCoroutine(AttackPlayer());
-//         }
+                if (direction.y > 0)
+                {
+                    direction = Vector3.up;
+                }
+                else
+                {
+                    direction = Vector3.down;
+                }
+            }
+        }
 
-//     }
-// }
+        // 現在の向きを設定
+        currentDirection = direction;
+
+        // 目的地に敵が居るか判定
+        Collider2D hit = Physics2D.OverlapPoint(transform.position + currentDirection, PlayerMask);
+
+        if (hit != null) // 何か居たら
+        {
+            if (hit.CompareTag("Player")) // プレイヤーならば
+            {
+                // 行動中かどうか
+                isMoving = true;
+
+                // 相手の被ダメージメソッドを取得
+                DealDamage dealDamage = hit.GetComponent<DealDamage>();
+
+                // 相手がダメージメソッドを持っていれば呼び出し
+                if (dealDamage != null)
+                {
+                    dealDamage.Damage(attackPower);
+                    Debug.Log("dealDamage呼び出し");
+                }
+
+                // エフェクトを再生
+                ParticleSystem attackParticle = Instantiate(attackEffect, transform.position + (currentDirection * 2 / 3), Quaternion.identity);
+                Debug.Log("エフェクト再生");
+                attackParticle.Play();
+
+                // クールタイム終了まで待機する
+                yield return new WaitForSeconds(attackCoolTime);
+
+                // 行動中フラグを戻す
+                isMoving = false;
+
+                // エフェクトオブジェクトを削除
+                Destroy(attackParticle.gameObject);
+            }
+        }
+        else
+        {
+            Debug.Log("攻撃対象が見つかりませんでした");
+        }
+    }
+
+    // void ResetAnimation()
+    // {
+    //     animator.SetBool("WalkDown", false);
+    //     animator.SetBool("WalkUp", false);
+    //     animator.SetBool("WalkLeft", false);
+    //     animator.SetBool("WalkRight", false);
+    // }
+
+}
