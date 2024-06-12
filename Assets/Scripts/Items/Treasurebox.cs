57e866662f8e73699bdfdfb82ab9e38c5aa89a5f
sishikawa-rop2312 <155729870+sishikawa-rop2312@users.noreply.github.com>
2024-06-12 17:22:35 +0900

    宝箱実装中

--- Assets/Scripts/Items/Treasurebox.cs
+++ Assets/Scripts/Items/Treasurebox.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class Treasurebox : MonoBehaviour
 {
     Animator animator;
+    SpriteRenderer spriteRenderer;
     bool isOpen = false;
+    PlayerController playerController;
+
+    public LayerMask detectionMask;
+    public GameObject itemPrefab; // 出現させるアイテムのプレハブ
+    public float fadeDuration = 1f; // フェードアウトの時間
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        playerController = FindObjectOfType<PlayerController>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !isOpen)
+        if (Input.GetKeyDown(KeyCode.Space) && !isOpen && IsPlayerOpenTreasurebox())
         {
             OpenTreasurebox();
         }
@@ -24,5 +32,46 @@ public class Treasurebox : MonoBehaviour
     {
         animator.SetTrigger("OpenTrigger");
         isOpen = true;
+        StartCoroutine(FadeOutAndSpawnItem());
+    }
+
+    // プレイヤーの向いてる1マス前に宝箱存在するか
+    bool IsPlayerOpenTreasurebox()
+    {
+        Collider2D hit = Physics2D.Raycast(playerController.transform.position, playerController.currentDirection, 1f, detectionMask).collider;
+        if (hit != null && hit.CompareTag("Treasurebox"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    IEnumerator FadeOutAndSpawnItem()
+    {
+        float startAlpha = spriteRenderer.color.a;
+        float rate = 1.0f / fadeDuration;
+        float progress = 0.0f;
+
+        while (progress < 1.0f)
+        {
+            Color tmpColor = spriteRenderer.color;
+            tmpColor.a = Mathf.Lerp(startAlpha, 0, progress);
+            spriteRenderer.color = tmpColor;
+            progress += rate * Time.deltaTime;
+
+            yield return null;
+        }
+
+        // 最終的に完全に透明にする
+        Color finalColor = spriteRenderer.color;
+        finalColor.a = 0;
+        spriteRenderer.color = finalColor;
+
+        // アイテムを出現させる
+        Instantiate(itemPrefab, transform.position, Quaternion.identity);
+
+        // 宝箱オブジェクトを非アクティブにするか破棄する
+        gameObject.SetActive(false);
+        // またはDestroy(gameObject); // 完全に破棄する場合
     }
 }
