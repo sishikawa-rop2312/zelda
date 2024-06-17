2741621a27728cc762dfe59cf10847b4defe411d
Morning-KiNYa <morning.master765@gmail.com>
2024-06-17 18:25:54 +0900

    HPがMAXの時はポーション消費しなiよう修正

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -31,7 +31,7 @@ public class ItemManager : MonoBehaviour
 
     public void UseHealthPotion()
     {
-        if (healthPotionCount > 0)
+        if (healthPotionCount > 0 && dealDamage.hp != dealDamage.maxHp)
         {
             dealDamage.Heal(1.0f);
             healthPotionCount--;
