5a47c2b7149ac02d9f4ffe0e0cce64c5951931ea
harukasato0215 <popo02150125@gmail.com>
2024-06-19 17:00:08 +0900

    BGM少し下げました。SEも少し変わってます

--- Assets/Scripts/Status/HelpManager.cs
+++ Assets/Scripts/Status/HelpManager.cs
@@ -10,6 +10,10 @@ public class HelpManager : MonoBehaviour
     // HelpManager型のインスタンスを保持
     public static HelpManager instance;
 
+    public AudioClip helpShowSound;
+    public AudioClip helpHideSound;
+    private AudioSource audioSource;
+
     void Awake()
     {
         if (instance == null)
@@ -24,6 +28,7 @@ public class HelpManager : MonoBehaviour
             // 2回目以降重複して作成してしまったgameObjectを削除
             Destroy(gameObject);
         }
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Start()
@@ -48,6 +53,7 @@ public class HelpManager : MonoBehaviour
 
     void ShowHelp()
     {
+        audioSource.PlayOneShot(helpShowSound);
         helpPanel.SetActive(true);
         Time.timeScale = 0f; // ゲームを一時停止
         isHelpActive = true;
@@ -55,6 +61,7 @@ public class HelpManager : MonoBehaviour
 
     void HideHelp()
     {
+        audioSource.PlayOneShot(helpHideSound);
         helpPanel.SetActive(false);
         Time.timeScale = 1f; // ゲームを再開
         isHelpActive = false;
